40b821166004d60968a5343b5c280b9cb93f0f50
Vigo Vlugt <vigovlugt@gmail.com>
2017-02-27 14:57:02 +0100

    redirect

--- menu.js
+++ menu.js
@@ -12,12 +12,8 @@ window.onload = function() {
 
 function accountFunc() {
   var provider = new firebase.auth.GoogleAuthProvider();
-  firebase.auth().signInWithPopup(provider).then(function(result) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = result.credential.accessToken;
-    // The signed-in user info.
-    var user = result.user;
-    // ...
+  firebase.auth().signInWithRedirect(provider).then(function(result) {
+        alert("logged in")
   }).catch(function(error) {
     // Handle Errors here.
     alert(error.message);
