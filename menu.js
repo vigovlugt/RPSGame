ea9fcd005d29a4ab126d75613bd174fb3165a28a
Vigo Vlugt <vigovlugt@gmail.com>
2017-02-27 14:39:24 +0100

    v0.01
    very bugs such glitch

---
+++ menu.js
@@ -0,0 +1,37 @@
+
+
+
+
+
+const titles = document.getElementsByClassName('title');
+window.onload = function() {
+    titles[0].innerHTML = "tst"
+    var ui = new firebaseui.auth.AuthUI(firebase.auth());
+}
+
+
+function accountFunc() {
+  var provider = new firebase.auth.FacebookAuthProvider();
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+  }).catch(function(error) {
+    // Handle Errors here.
+    alert(error.message);
+  });
+
+
+
+
+
+  firebase.auth().onAuthStateChanged(function(user) { 
+        if (user) {   // User is signed in.
+          alert(firebase.auth().currentUser)
+             } else {   // No user is signed in.
+
+            }
+    });
+  }
