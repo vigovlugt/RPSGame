d5b8f86ca5af9171550f2a7f87427af58ac8880e
Vigo Vlugt <vigovlugt@gmail.com>
2017-02-27 17:38:13 +0100

    Friends and pages working

--- menu.js
+++ menu.js
@@ -5,50 +5,34 @@ $(document).ready(function() {   
 
 
 function accountFunc() {
-  console.log(firebase.auth().currentUser)
+    console.log(firebase.auth().currentUser)
     if (firebase.auth().currentUser == null) {
         <!-- if there is no user logged in-->
         var provider = new firebase.auth.GoogleAuthProvider();
         firebase.auth().signInWithRedirect(provider).then(function(result) {
             alert("logged in")
         }).catch(function(error) {
-                // Handle Errors here.
-                alert(error.message);
-            })} else {
-                loadPage(4);
-                alert("Loading account page");
-            }
+            // Handle Errors here.
+            alert(error.message);
+        })
+    } else {
+        loadPage(4);
+        alert("Loading account page");
+    }
 
 
-        }
+}
 
 
-    firebase.auth().onAuthStateChanged(function(user) { 
-        if (user) {   // User is signed in.
-          console.log(user);
-        } else {   // No user is signed in.
+firebase.auth().onAuthStateChanged(function(user) { 
+    if (user) {   // User is signed in.
+        console.log(user);
+    } else {   // No user is signed in.
 
-        }
-    });
+    }
+});
 
 function loadPage(_page) {
     $(".page").hide();
-    switch (_page) {
-        case 0:     // overview
-
-            break;
-        case 1:    // play
-
-            break;
-        case 2:     // friends
-
-            break;
-        case 3:     // inventory
-
-            break;
-        case 4  :   // account
-
-            break;
-    }
-
+    $("." + _page).show();
 }
